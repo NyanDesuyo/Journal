8370779ba6c061238064c6e1f6fb11cf6f40e185
NyanDesuyo <gerogeroe12@gmail.com>
2023-02-02 17:44:04 +0700

    Ignore refresh token and revoke token White List

--- server/src/constant/index.ts
+++ server/src/constant/index.ts
@@ -11,6 +11,8 @@ export const DEFAULT_TAKE_VALUE = 10;
 
 export const PATH_WHITELIST: string[] = [
   "/status",
+  "/v1/auth/refresh-token",
+  "/v1/auth/revoke-refresh-token",
   "/v1/auth/register",
   "/v1/auth/login",
   "/graphql",
